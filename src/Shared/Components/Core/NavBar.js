f9234b1629b31e48762a324eb7d40a62fdf69f07
mrnabdulai <mohammedrafiq.dev@gmail.com>
2023-03-03 15:45:27 +0000

    Replaced imgs with avatars

--- src/Shared/Components/Core/NavBar.js
+++ src/Shared/Components/Core/NavBar.js
@@ -26,11 +26,11 @@ export default function NavBar() {
                 <AdjustmentsHorizontalIcon className='w-6 h-6  cursor-pointer text-primary' />
                 <BellIcon className='w-6 h-6 cursor-pointer text-primary' />
                 <DocumentIcon className='w-6 h-6 cursor-pointer text-primary' />
-                    <img
-                        className="inline-block h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                    />            
+                <span className="inline-block h-10 w-10 overflow-hidden rounded-full bg-gray-100">
+        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </span>           
                             {/* <p>{currentDateString}</p> */}
                 </div>
             </div>
--- src/Shared/Components/Core/SideBar/Sidebar.js
+++ src/Shared/Components/Core/SideBar/Sidebar.js
@@ -121,8 +121,11 @@ function Sidebar() {
     <hr className='mt-3'/>
     <div className='px-4 py-3 flex    justify-between items-center'>
       <div className='flex items-center gap-2'>
-        <img src={AVATAR_IMG_URL} className="object-cover w-10 h-10 bg-primary object-center  rounded-full mr-2" />
-      <div>
+      <span className="inline-block h-10 w-10 overflow-hidden rounded-full bg-gray-100">
+        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </span>      <div>
         <h1 className='text-sm font-medium text-black'>Profile Name</h1>
         <h3 className='text-xs text-[#92A1B1]'>Role</h3>
       </div>
